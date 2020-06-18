a794da368dafe2df8fbdc500ab13cfa023ac9067
Jojapimpa <galaktineee@gmail.com>
2020-06-18 20:57:48 +0300

    small directory refactor

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-require('module-alias/register');
-const http = require('http');
-const PotatoAPI = require('api/config/app.js');
-const PotatoServer = http.Server(PotatoAPI);
-const PotatoServerPORT = process.env.PORT || 3001;
-const LOCAL = '0.0.0.0';
 
-PotatoServer.listen(
-    PotatoServerPORT,
-    LOCAL,
-    () => console.log(`PotatoAPI running on ${PotatoServerPORT}`)
-);
-
+console.log('to run api: npm run service');
