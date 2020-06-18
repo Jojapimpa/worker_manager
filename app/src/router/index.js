2d13f010c4515a9d0126af7cb9da6ece43bb182f
Jojapimpa <galaktineee@gmail.com>
2020-06-18 21:35:50 +0300

    routes

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,10 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import * as Auth from '@/components/pages/auth'
+
+import auth from '@/components/pages/auth/auth'
+import homeAdmin from '@/components/pages/homeAdmin/homeAdmin'
+import homeSecretary from '@/components/pages/homeSecretary/homeSecretary'
+
 Vue.use(Router)
 
 const router = new Router({
   routes: [
+    {
+      path: '/admin',
+      name: 'admin',
+      component: homeAdmin,
+      meta: {
+        requiredAuth: true
+      }
+    },
+    {
+      path: '/secretary',
+      name: 'secretary',
+      component: homeSecretary,
+      meta: {
+        requiredAuth: true
+      }
+    },
     {
       path: '/login',
       name: 'auth',
@@ -12,8 +34,7 @@ const router = new Router({
       meta: {
         requiredNoAuth: true
       }
-    },
-
+    }
   ]
 })
 
@@ -22,12 +43,15 @@ router.beforeEach((to, from, next) => {
     return router.push('/login')
   }
 
+  if (to.meta.requiredNoAuth && Auth.default.checkAuthentication()) {
+    return router.push('/')
+  }
+
   if (to.path === '/') {
-    return router.push('/login')
+    return router.push(Auth.default.getType() === 'secretary' ? '/secretary' : '/admin')
   }
 
   next()
 })
-
 
 export default router
