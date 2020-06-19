343d1362ab64333b1a9dc9697a38cc1d912fbbe6
Jojapimpa <galaktineee@gmail.com>
2020-06-19 05:14:47 +0300

    default admin account

--- service/api/app/setup/index.js
+++ service/api/app/setup/index.js
@@ -2,9 +2,21 @@ const mongoose = require('mongoose');
 require('@Models/user');
 require('@Models/worker');
 
+const config = require('@config');
+
 const models = {
     User: mongoose.model('User'),
     Worker: mongoose.model('Worker'),
 };
+
+let admin = new models.User({
+    username: 'admin',
+    password: config.default_admin_pass,
+    type: 'admin',
+    fio: "Администратор",
+    email: 'admin@admin'
+})
+
+admin.save().catch(err => {});
 
 module.exports = models;
--- service/api/config/index.js
+++ service/api/config/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     secret: 'somesecret',
+    default_admin_pass: 'admin',
     session: { session: false },
     database: 'mongodb://localhost:27017/worker_manager',
 };
