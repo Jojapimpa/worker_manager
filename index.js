2b986b39c33d6961ba93c66aba3ab5ded8caf655
Jojapimpa <galaktineee@gmail.com>
2020-06-19 05:45:57 +0300

    start scripts

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-console.log('to run api: npm run service');
+console.log(
+    `
+----------------------
+    Worker Manager
+----------------------`
+);
+
+console.log('install: \x1b[32m%s\x1b[0m','npm run install');
+console.log('api: \x1b[32m%s\x1b[0m', 'npm run backend');
+console.log('frontend: \x1b[32m%s\x1b[0m', 'npm run frontend');
+
+console.log('');
